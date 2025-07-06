7d682f857f4ea49e7624458444080ec0de4f7859
nmbath <mark@baggywrinkle.co.uk>
2025-07-06 18:53:40 +0100

    Added Tailscale start/stop capability

--- src/application.hpp
+++ src/application.hpp
@@ -80,6 +80,7 @@ protected slots:
 	void onCanInterfacesChanged();
 	void onDemoSettingChanged(QVariant var);
 	void onEvccSettingChanged(QVariant var);
+	void onTailscaleSettingChanged(QVariant var);
 	void onLanguageChanged(QVariant var);
 	void onLocalSettingsStateChanged(VeQItem::State state);
 	void onLocalSettingsTimeout();
@@ -134,6 +135,8 @@ private:
 
 	DaemonToolsService *mGeneratorStarter = nullptr;
 	DaemonToolsService *mParallelBmsStarter = nullptr;
+	DaemonToolsService *mTailscaleBackend = nullptr;
+	DaemonToolsService *mTailscaleControl = nullptr;
 	QList<QString> mGeneratorStarterConditions;
 	QList<QString> mParallelBmsConditions;
 
