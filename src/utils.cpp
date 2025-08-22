10cdca0fc56d9c4e28c06e6065c6ea8bb79770cd
Wiebe Cazemier <wiebe@ytec.nl>
2025-08-22 16:10:51 +0200

    Implement long press: reset network
    This replaces the Python venus-button-handler, also fixes the following
    bugs (mostly by virtue of reboot, which was unavoidable):
    
    - vncpassword.txt is removed, but nobody re-created it.
    - The Bluetooth light didn't always come back on.
    - The wifi led keeps blinking fast.
    
    The actions were put in a separate executable because venus-platform
    responds to what we're doing with settings, and doesn't act nicely when
    we're gracefully shutting down localsettings.

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,4 +1,6 @@
 #include <veutil/qt/ve_qitem.hpp>
+#include <QFile>
+#include <thread>
 
 void setWifiHotspotAndBluetooth(VeQItemSettings *settings, const int val)
 {
@@ -17,4 +19,58 @@ void setWifiHotspotAndBluetooth(VeQItemSettings *settings, const int val)
 		qDebug() << "Setting Bluetooth to:" << val;
 		bluetooth->setValue(val);
 	}
+}
+
+bool serviceRunning(QString const &svc, bool *ok) {
+	*ok = false;
+	QString statusFile = QString("/service/%1/supervise/status").arg(svc);
+
+	QFile file(statusFile);
+
+	if (!file.exists())
+		return false;
+
+	if (!file.open(QIODevice::ReadOnly))
+		return false;
+
+	const QByteArray statusData = file.readAll();
+	file.close();
+
+	if (statusData.size() < 16)
+		return false;
+
+	quint32 pid = 0;
+
+	// https://github.com/daemontools/daemontools/blob/master/src/svstat.c
+	pid = static_cast<unsigned char>(statusData[15]);
+	pid <<= 8; pid += static_cast<unsigned char>(statusData[14]);
+	pid <<= 8; pid += static_cast<unsigned char>(statusData[13]);
+	pid <<= 8; pid += static_cast<unsigned char>(statusData[12]);
+
+	*ok = true;
+	return pid != 0;
+}
+
+/**
+ * Note that this blocks, so only use when you know you can afford that.
+ */
+bool stopServiceAndWaitForExit(const QString &svc, const std::chrono::seconds timeout)
+{
+	bool stopped = false;
+
+	for (auto limit = std::chrono::steady_clock::now() + timeout ; std::chrono::steady_clock::now() < limit; )
+	{
+		bool ok = false;
+		bool running = serviceRunning(svc, &ok);
+
+		if (ok && !running) {
+			qDebug() << svc << "exit detected";
+			stopped = true;
+			break;
+		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+
+	return stopped;
 }
