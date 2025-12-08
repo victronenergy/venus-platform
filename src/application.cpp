3bd311ba3558f018120b893e28e56b469b35ea24
Wiebe Cazemier <wiebe@ytec.nl>
2025-12-08 16:35:53 +0100

    button-handler: double press: enable pairing mode
    If a dedicated button on GX is pressed, pairing is allowed
    temporarily without a password.
    
    https://github.com/victronenergy/venus/issues/1535
    
    Co-authored-by: Jeroen Hofstee <jhofstee@victronenergy.com>

--- src/application.cpp
+++ src/application.cpp
@@ -914,7 +914,14 @@ void Application::onButtonShortPress()
 
 void Application::onButtonDoublePress()
 {
+	auto *item = mService->itemGet("Tokens/Pairing/Enable");
 
+	if (!item) {
+		qCritical() << "[button] Tokens/Pairing/Enable not found";
+		return;
+	}
+
+	item->setValue(1);
 }
 
 void Application::onButtonLongPress()
